ff185d78b3e2ebe1f2ab4eddf67dda1b9678e29c
emreyurur <emreyrr81@gmail.com>
2024-03-24 19:16:24 +0300

    DetailScreen de degisiklikler yapildi.

--- src/screens/ContactsDetailScreen.tsx
+++ src/screens/ContactsDetailScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, TextInput, Button } from 'react-native';
 import { Contact } from 'react-native-contacts';
 
 interface ContactDetailProps {
@@ -18,6 +18,7 @@ const ContactsDetailScreen: React.FC<ContactDetailProps> = ({ route }) => {
   const emailIcon = require('../assets/email.png');
   const messageIcon = require('../assets/message.png');
   const pointsIcon = require('../assets/points.png');
+  const solanaIcon = require('../assets/solana.webp');
 
   if (!contact) {
     return (
@@ -26,6 +27,12 @@ const ContactsDetailScreen: React.FC<ContactDetailProps> = ({ route }) => {
       </View>
     );
   }
+
+  const handleSaveAddress = (address: string) => {
+    // Burada Solana adresini kaydetme işlemleri yapılabilir
+    console.log('Saved Solana address:', address);
+    // Örnek olarak konsola kaydedildiğini gösterelim
+  };
 
   return (
     <ScrollView style={styles.container}>
@@ -51,16 +58,24 @@ const ContactsDetailScreen: React.FC<ContactDetailProps> = ({ route }) => {
         <Text style={styles.infoText}>{contact.phoneNumbers[0]?.number}</Text>
       </View>
 
-      {/* E-posta adresi için benzer bir yapı kurulabilir */}
       <View style={styles.infoContainer}>
         <Image source={emailIcon} style={styles.emailIcon} />
         <Text style={styles.infoText}>{contact.emailAddresses[0]?.email}</Text>
-        {/* <View style={styles.solanaInput}>
-            <TextInput
-            placeholder='Enter your Solana address'
-            />
-        </View> */}
       </View>
+
+      <View style={styles.solanaContainer}>
+  <Image source={solanaIcon} style={styles.solanaIcon} />
+  <TextInput
+    style={styles.solanaInput}
+    placeholder='Enter your Solana address'
+    placeholderTextColor='#7f8c8d' // Gri ton bir placeholder rengi
+    // onChangeText={setSolanaAddress} // Girilen değeri state'e kaydet
+    // value={solanaAddress} // TextInput'un değeri
+  />
+  <TouchableOpacity style={styles.saveButton} onPress={handleSaveAddress}>
+    <Text style={styles.saveButtonText}>Save</Text>
+  </TouchableOpacity>
+</View>
     </ScrollView>
   );
 };
@@ -71,22 +86,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   imageContainer: {
-    backgroundColor: '#e0e0e0', // Fotoğrafın yerini alacak varsayılan arkaplan rengi.
-    position: 'relative', // İçerideki mutlak pozisyonlu öğeleri konumlandırmak için.
+    backgroundColor: '#e0e0e0',
+    position: 'relative',
   },
   contactImage: {
-    width: '100%', // Genişliği ekranın genişliği kadar yap.
-    aspectRatio: 16 / 12, // Yükseklik/genişlik oranı.
+    width: '100%',
+    aspectRatio: 16 / 12,
   },
   name: {
     fontSize: 22,
     fontWeight: 'bold',
     color: '#000',
     textAlign: 'center',
-    marginTop: -30, // İsim metnini fotoğrafın üzerine çekmek için.
-    backgroundColor: '#fff', // İsim metni arkaplanı.
-    width: '100%', // Genişliği ekranın genişliği kadar yap.
-    padding: 10, // İsim metni için padding.
+    marginTop: -30,
+    backgroundColor: '#fff',
+    width: '100%',
+    padding: 10,
   },
   infoContainer: {
     flexDirection: 'row',
@@ -107,17 +122,17 @@ const styles = StyleSheet.create({
   },
   favoriteButton: {
     position: 'absolute',
-    top: 20, // Yukarıdan boşluk.
-    right: 60, // Sağdan boşluk.
-    borderRadius: 15, // Düğme yuvarlaklaştırması.
-    padding: 5, // İkon çevresinde padding.
+    top: 20,
+    right: 60,
+    borderRadius: 15,
+    padding: 5,
   },
   pointsButton: {
     position: 'absolute',
-    top: 20, // Yukarıdan boşluk.
-    right: 20, // Sağdan boşluk.
-    borderRadius: 15, // Düğme yuvarlaklaştırması.
-    padding: 5, // İkon çevresinde padding.
+    top: 20,
+    right: 20,
+    borderRadius: 15,
+    padding: 5,
   },
   favoriteIcon: {
     width: 30,
@@ -131,7 +146,54 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
     marginRight: 10,
-    marginLeft:10
+    marginLeft: 10,
+  },
+  solanaContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff', // Beyaz arkaplan
+    borderRadius: 5, // Köşeleri yuvarlat
+    margin: 10,
+    marginTop:10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 5, // Android için gölge efekti
+  },
+  solanaIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  solanaInput: {
+    flex: 1,
+    height: 40, // Yeterli alan sağlamak için
+    borderColor: '#3d58d1', // Solana mavisi tonu
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 16,
+    color: '#2c3e50', // Karanlık gri metin rengi
+  },
+  saveButton: {
+    backgroundColor: '#3d58d1', // Solana mavisi tonu
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginLeft: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 5, // Android için gölge efekti
+  },
+  saveButtonText: {
+    color: '#fff', // Beyaz metin
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
