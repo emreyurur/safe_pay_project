05f01773acd2224d7bff6cce53a9ae8043059412
emreyurur <emreyrr81@gmail.com>
2024-03-25 20:40:25 +0300

    Phantom wallet entegrasyonu yapildi.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
